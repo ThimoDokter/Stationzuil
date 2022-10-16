2dd66fcc5f299c6bba53e55632dad69c5b689209
ThimoDokter <thimo.dokter@student.hu.nl>
2022-10-16 23:49:44 +0200

    added docstrings

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import time
 import random
 
 print("Welkom, Om een review achter te laten vragen wij u graag om een aantal gegevens")
-#onderstaande functie, vraagt de gegevens van de gebruiker en returnd
 def gebruikers_vragen():
+    """
+    vraagt gegevens van de gebruiker zoals naam en bericht, deze gegevens worden gecontroleerd op lengte en naam.
+    returns:
+    returned de gegevens in een list deze list is zo opgebouwd [naam, bericht, tijd]
+    """
     i = 0
     naam = input("Voer alstublieft uw naam in, als uw geen naam in vult word het bericht als anoniem geplaatst")
     if naam == "":
@@ -30,10 +34,16 @@ def gebruikers_vragen():
 gebruikers_vragen()
 
 def randomstation():
+    """"
+    kiest een random station uit de lijst
+    returned:
+    een random station uit de lijst
+    """
     i = 0
     while i == 0:
         stations = ['Arnhem','Almere', 'Amersfoort', 'Almelo', 'Alkmaar', 'Apeldoorn', 'Assen', 'Amsterdam', 'Boxtel', 'Breda', 'Dordrecht', 'Delft', 'Deventer', 'Enschede', 'Gouda', 'Groningen', 'Haarlem', 'Helmond', 'Hoorn', 'Heerlen', 'Den Bosch', 'Hilversum', 'Leiden', 'Lelystad', 'Leeuwarden', 'Maastricht', 'Nijmegen', 'Oss', 'Roermond', 'Roosendaal', 'Sittard', 'Tilburg', 'Utrecht', 'Venlo', 'Vlissingen', 'Zaandam', 'Zwolle', 'Zutphen']
         station = stations[random.randrange(1,38)]
         print(station)
         i = 1
+    return station
 randomstation()
